d2ab0400cc38898da2a12dc63ad1351a2a7c2874
Philippe Muller <philippe.muller@gmail.com>
2022-02-26 18:45:59 +0100

    fix: Avoid CLS by setting a static height on social network icons

--- packages/website/src/components/SocialNetworkIcon.tsx
+++ packages/website/src/components/SocialNetworkIcon.tsx
@@ -3,10 +3,19 @@ import React from "react";
 type Props = {
   name: string;
   url: string;
+  size?: string;
 };
 
-export const SocialNetworkIcon: React.FC<Props> = ({ name, url }) => (
+export const SocialNetworkIcon: React.FC<Props> = ({
+  name,
+  url,
+  size = "48px",
+}) => (
   <a target="_blank" rel="noopener" href={url}>
-    <img style={{ width: "48px" }} src={`/icons/${name}.svg`} alt={name} />
+    <img
+      style={{ width: size, height: size }}
+      src={`/icons/${name}.svg`}
+      alt={name}
+    />
   </a>
 );
