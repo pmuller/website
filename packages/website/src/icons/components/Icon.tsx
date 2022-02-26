b860edb82a505373fdc42dfa16d355d71e69501e
Philippe Muller <philippe.muller@gmail.com>
2022-02-26 20:55:19 +0100

    Make social network icons responsive

--- packages/website/src/icons/components/Icon.tsx
+++ packages/website/src/icons/components/Icon.tsx
@@ -1,17 +1,15 @@
 import React from "react";
 
+import { container } from "./Icon.module.css";
+
 type Props = {
   name: string;
   url: string;
   path: string;
-  size?: string;
 };
 
-export const Icon: React.FC<Props> = (props) => {
-  const { name, path, url, size = "48px" } = props;
-  return (
-    <a target="_blank" rel="noopener" href={url}>
-      <img style={{ width: size, height: size }} src={path} alt={name} />
-    </a>
-  );
-};
+export const Icon: React.FC<Props> = ({ name, path, url }) => (
+  <a target="_blank" rel="noopener" href={url}>
+    <img className={container} src={path} alt={name} />
+  </a>
+);
