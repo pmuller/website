34b336673bdd3a320f27b6964fc55d5768c1ee12
Philippe Muller <philippe.muller@gmail.com>
2022-02-26 19:27:07 +0100

    Add Avatar

---
+++ packages/website/src/components/Avatar.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Avatar: React.FC = () => (
+  <img style={{ width: "48px", height: "48px" }} src="/images/pmuller.png" />
+);
--- packages/website/src/components/Header.tsx
+++ packages/website/src/components/Header.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
+import { Avatar } from "./Avatar";
 import { container } from "./Header.module.css";
 import { SocialNetworkSection } from "./SocialNetworkSection";
 import { Title } from "./Title";
 
 export const Header: React.FC = () => (
   <header className={container}>
+    <Avatar />
     <Title />
     <SocialNetworkSection />
   </header>
