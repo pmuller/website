ef28725a8a189079411514748f3f95740c23d27b
Philippe Muller <philippe.muller@gmail.com>
2022-02-27 19:47:39 +0100

    Highlight important words

--- packages/website/src/components/Introduction.tsx
+++ packages/website/src/components/Introduction.tsx
@@ -4,8 +4,13 @@ import { container } from "./Introduction.module.css";
 
 export const Introduction: React.FC = () => (
   <section className={container}>
-    <p>Hi, I'm Philippe Muller.</p>
-    <p>I am a software engineer and I am fond of infrastructure automation.</p>
+    <p>
+      Hi, my name is <strong>Philippe Muller</strong>.
+    </p>
+    <p>
+      I am a <strong>software engineer</strong> and I am fond of{" "}
+      <strong>infrastructure automation</strong>.
+    </p>
     <p>
       I am currently building awesome team building games at{" "}
       <a target="_blank" rel="noopener" href="https://happyteams.events/en">
