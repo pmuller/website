bda3cbfa3377f546ecf6b1a3a44b55c2fb6a8a0c
Philippe Muller <philippe.muller@gmail.com>
2022-03-02 15:23:15 +0100

    Remove useless type parameter

--- packages/website/src/pages/index.tsx
+++ packages/website/src/pages/index.tsx
@@ -1,10 +1,9 @@
-import { PageProps } from "gatsby";
 import React from "react";
 
 import { Layout } from "../core";
 import { Introduction } from "./Index";
 
-const Index: React.FC<PageProps> = () => (
+const Index: React.FC = () => (
   <Layout>
     <Introduction />
   </Layout>
