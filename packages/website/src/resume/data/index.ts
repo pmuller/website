9c20a4702031e170d11aa719e4fe6a4c931b617e
Philippe Muller <philippe.muller@gmail.com>
2022-03-03 11:39:18 +0100

    Export education and experience data

--- packages/website/src/resume/data/index.ts
+++ packages/website/src/resume/data/index.ts
@@ -1,3 +1,5 @@
 export { contactMethods } from "./contactMethods";
+export { education } from "./education";
+export { experience } from "./experience";
 export { identity } from "./identity";
 export { summary } from "./summary";
