af4831560180d306ecb9bfe73fff4e4b32652b4a
Philippe Muller <philippe.muller@gmail.com>
2022-03-04 12:07:09 +0100

    Style contact methods

--- packages/website/src/resume/components/Contact.tsx
+++ packages/website/src/resume/components/Contact.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 import { contactMethods } from "../data";
+import { container } from "./Contact.module.css";
 import { ContactMethod } from "./ContactMethod";
 
 export const Contact: React.FC = () => (
-  <div>
+  <div className={container}>
     {contactMethods.map((method, index) => (
       <ContactMethod {...method} key={index} />
     ))}
--- packages/website/src/resume/components/ContactMethod.tsx
+++ packages/website/src/resume/components/ContactMethod.tsx
@@ -2,12 +2,11 @@ import Icon from "@mdi/react";
 import React from "react";
 
 import { ContactMethod as Props } from "../types";
+import { container } from "./ContactMethod.module.css";
 
 export const ContactMethod: React.FC<Props> = ({ label, url, icon }) => (
-  <div>
+  <a href={url} rel="noopener" target="_blank" className={container}>
+    {label}
     <Icon path={icon} size={1} />
-    <a href={url} rel="noopener" target="_blank">
-      {label}
-    </a>
-  </div>
+  </a>
 );
