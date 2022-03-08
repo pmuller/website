f73132a404961a203668462f95689a55e66f5b81
Philippe Muller <philippe.muller@gmail.com>
2022-03-08 15:09:55 +0100

    s/Clone/Fork/

--- packages/website/src/core/components/Footer.tsx
+++ packages/website/src/core/components/Footer.tsx
@@ -4,7 +4,7 @@ import { FooterContainer } from "./FooterContainer";
 
 export const Footer: React.FC = () => (
   <FooterContainer>
-    © {new Date().getFullYear()} Philippe Muller - Clone on{" "}
+    © {new Date().getFullYear()} Philippe Muller - Fork on{" "}
     <a rel="noopener" target="_blank" href="https://github.com/pmuller/website">
       GitHub
     </a>
