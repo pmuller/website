21d778582f6fe5e18ea5631d11526a4147c131a8
Philippe Muller <philippe.muller@gmail.com>
2022-03-10 12:05:45 +0100

    Manage global CSS with Linaria

--- packages/website/gatsby-browser.js
+++ packages/website/gatsby-browser.js
@@ -1,2 +1,23 @@
-/* eslint-disable import/no-unassigned-import,import/no-unused-modules,import/no-internal-modules */
-import "./src/css/global.css";
+/* eslint-disable import/no-unused-modules,no-unused-expressions */
+import { css } from "linaria";
+
+css`
+  /* stylelint-disable-next-line selector-pseudo-class-no-unknown */
+  :global() {
+    body {
+      margin: 0;
+    }
+
+    a,
+    a:visited {
+      color: #666;
+      text-underline-offset: 0.1rem;
+      text-decoration-style: dotted;
+    }
+
+    a:hover {
+      color: #000;
+      text-decoration: none;
+    }
+  }
+`;
