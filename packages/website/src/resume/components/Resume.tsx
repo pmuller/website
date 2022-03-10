e440d9f0e8bd913b678ad59110e19a31e1a1577c
Philippe Muller <philippe.muller@gmail.com>
2022-03-10 15:25:46 +0100

    Add ResumeContainer

--- packages/website/src/resume/components/Resume.tsx
+++ packages/website/src/resume/components/Resume.tsx
@@ -3,13 +3,14 @@ import React from "react";
 import { Education } from "./Education";
 import { Experience } from "./Experience";
 import { Header } from "./Header";
+import { ResumeContainer } from "./ResumeContainer";
 import { Summary } from "./Summary";
 
 export const Resume: React.FC = () => (
-  <div>
+  <ResumeContainer>
     <Header />
     <Summary />
     <Experience />
     <Education />
-  </div>
+  </ResumeContainer>
 );
---
+++ packages/website/src/resume/components/ResumeContainer.tsx
@@ -0,0 +1,5 @@
+import { styled } from "linaria/react";
+
+export const ResumeContainer = styled.div`
+  font-family: sans-serif;
+`;
