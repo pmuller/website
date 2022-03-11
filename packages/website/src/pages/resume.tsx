2a35d1ec2f68ae2f6e3bfe932be4e92149edd4fc
Philippe Muller <philippe.muller@gmail.com>
2022-03-11 16:37:54 +0100

    Remove page layout from resume

--- packages/website/src/pages/resume.tsx
+++ packages/website/src/pages/resume.tsx
@@ -1,12 +1,3 @@
-import React from "react";
-
-import { Layout } from "../core";
 import { Resume } from "../resume";
 
-const ResumePage: React.FC = () => (
-  <Layout>
-    <Resume />
-  </Layout>
-);
-
-export default ResumePage;
+export default Resume;
