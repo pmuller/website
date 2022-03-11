94bee1ee9e484e2dea9eef47ad434a82cc5e0413
Philippe Muller <philippe.muller@gmail.com>
2022-03-11 17:00:45 +0100

    Use sans-serif as default font

--- packages/website/src/core/components/BasePage.tsx
+++ packages/website/src/core/components/BasePage.tsx
@@ -3,4 +3,5 @@ import { styled } from "linaria/react";
 export const BasePage = styled.main`
   max-width: 1024px;
   margin: 0 auto;
+  font-family: sans-serif;
 `;
--- packages/website/src/resume/components/Layout.tsx
+++ packages/website/src/resume/components/Layout.tsx
@@ -1,7 +1,6 @@
 import { styled } from "linaria/react";
 
 export const Layout = styled.div`
-  font-family: sans-serif;
   margin: 0.5rem;
   display: grid;
 `;
