50627c1d27e8bb7362c89670eb32ea94958c50b4
Philippe Muller <philippe.muller@gmail.com>
2022-03-12 10:43:48 +0100

    Show job title after company name

--- packages/website/src/resume/components/ExperienceStage.tsx
+++ packages/website/src/resume/components/ExperienceStage.tsx
@@ -6,11 +6,11 @@ import { ExperienceStageDates } from "./ExperienceStageDates";
 
 export const ExperienceStage: React.FC<Props> = (props) => (
   <ExperienceStageContainer>
-    <div className="title">{props.title}</div>
     <div className="company">
       <div className="name">{props.company.name}</div>
       <div className="location">({props.location})</div>
     </div>
+    <div className="title">{props.title}</div>
     <ExperienceStageDates start={props.startDate} end={props.endDate} />
     <div className="description">{props.description}</div>
   </ExperienceStageContainer>
