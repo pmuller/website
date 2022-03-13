9c969cf0b1349fe79e47f1986f0fe809795ab37e
Philippe Muller <philippe.muller@gmail.com>
2022-03-13 18:18:21 +0100

    Make Dates and OrganizationLocation look the same

--- packages/website/src/resume/components/DatesContainer.tsx
+++ packages/website/src/resume/components/DatesContainer.tsx
@@ -2,6 +2,5 @@ import { styled } from "linaria/react";
 
 export const DatesContainer = styled.div`
   display: flex;
-  color: #555;
   gap: 0.2rem;
 `;
--- packages/website/src/resume/components/OrganizationLocationContainer.tsx
+++ packages/website/src/resume/components/OrganizationLocationContainer.tsx
@@ -2,6 +2,7 @@ import { styled } from "linaria/react";
 
 export const OrganizationLocationContainer = styled.span`
   display: flex;
+  gap: 0.2rem;
 
   & > svg {
     align-self: center;
