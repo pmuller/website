3e3cd27fe69b6aa667b8966a35fb7c8abd4ca233
Philippe Muller <philippe.muller@gmail.com>
2022-03-14 22:49:47 +0100

    Fix education dates alignment

--- packages/website/src/resume/components/EducationStageContainer.tsx
+++ packages/website/src/resume/components/EducationStageContainer.tsx
@@ -1,6 +1,7 @@
 import { styled } from "linaria/react";
 
 import { OrganizationContainer } from "./OrganizationContainer";
+import { OrganizationLocationContainer } from "./OrganizationLocationContainer";
 
 export const EducationStageContainer = styled.div`
   display: flex;
@@ -15,5 +16,11 @@ export const EducationStageContainer = styled.div`
   & > ${OrganizationContainer} {
     gap: 0.25rem;
     margin-bottom: 0;
+    display: flex;
+    flex-direction: column;
+
+    & > ${OrganizationLocationContainer} {
+      justify-content: flex-start;
+    }
   }
 `;
