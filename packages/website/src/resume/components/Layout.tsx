2e6f8a9259b75f2f4eb8f94b7597427224024221
Philippe Muller <philippe.muller@gmail.com>
2022-03-14 22:44:09 +0100

    Increase resume bottom margin

--- packages/website/src/resume/components/Layout.tsx
+++ packages/website/src/resume/components/Layout.tsx
@@ -1,7 +1,7 @@
 import { styled } from "linaria/react";
 
 export const Layout = styled.div`
-  margin: 0.5rem;
+  margin: 0.5rem 0.5rem 3rem 0.5rem;
   display: grid;
   gap: 1rem;
   grid-template-columns: 4fr 1fr;
