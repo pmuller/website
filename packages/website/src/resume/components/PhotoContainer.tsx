9065f0824bcdd3a5fe59f66509a03a5fc264a4fa
Philippe Muller <philippe.muller@gmail.com>
2022-03-14 21:35:12 +0100

    Reduce photo size on small screens

--- packages/website/src/resume/components/PhotoContainer.tsx
+++ packages/website/src/resume/components/PhotoContainer.tsx
@@ -3,4 +3,9 @@ import { styled } from "linaria/react";
 export const PhotoContainer = styled.img`
   border-radius: 50%;
   border: solid 3px var(--color-primary);
+  width: 80px;
+
+  @media (min-width: 480px) {
+    width: auto;
+  }
 `;
