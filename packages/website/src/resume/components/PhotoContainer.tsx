041b164b91c41c5268df408f029021df5dbcce8a
Philippe Muller <philippe.muller@gmail.com>
2022-03-14 23:22:49 +0100

    fix: Define Photo height to avoid CLS

--- packages/website/src/resume/components/PhotoContainer.tsx
+++ packages/website/src/resume/components/PhotoContainer.tsx
@@ -4,6 +4,7 @@ export const PhotoContainer = styled.img`
   border-radius: 50%;
   border: solid 3px var(--color-primary);
   width: 80px;
+  height: 80px;
 
   @media (min-width: 480px) {
     width: auto;
