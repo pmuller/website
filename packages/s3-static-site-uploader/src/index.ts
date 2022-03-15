a57d116cc657ee9fed01dcbdda24705143f1e09d
Philippe Muller <philippe.muller@gmail.com>
2022-03-15 09:50:09 +0100

    Basic hello world CLI

--- packages/s3-static-site-uploader/src/index.ts
+++ packages/s3-static-site-uploader/src/index.ts
@@ -1,1 +1,8 @@
-export const foo = 42;
+/* eslint-disable import/no-unused-modules */
+import { program } from "@caporal/core";
+
+program.action(({ logger }) => {
+  logger.info("Hello, world!");
+});
+
+program.run();
