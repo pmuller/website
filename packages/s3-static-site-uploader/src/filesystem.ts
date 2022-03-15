c06ba020a916d563ddedda8756420e17c9a0118d
Philippe Muller <philippe.muller@gmail.com>
2022-03-15 11:42:31 +0100

    Add recursive file listing logic

---
+++ packages/s3-static-site-uploader/src/filesystem.ts
@@ -0,0 +1,28 @@
+import { Logger } from "@caporal/core";
+import { readdir, stat } from "fs/promises";
+import { resolve } from "path";
+
+export const listFilesRecursively = async (
+  path: string,
+  logger: Logger
+): Promise<string[]> =>
+  (
+    await Promise.all(
+      (
+        await readdir(path)
+      ).map(async (name) => {
+        const fullPath = resolve(path, name);
+        const pathStats = await stat(fullPath);
+
+        if (pathStats.isDirectory())
+          return listFilesRecursively(fullPath, logger);
+
+        if (pathStats.isFile()) return [fullPath];
+
+        logger.warn(
+          `Ignoring ${fullPath} because it is not a directory nor a regular file`
+        );
+        return [];
+      })
+    )
+  ).flat();
--- packages/s3-static-site-uploader/src/index.ts
+++ packages/s3-static-site-uploader/src/index.ts
@@ -1,16 +1,21 @@
 /* eslint-disable import/no-unused-modules */
+import { listFilesRecursively } from "./filesystem";
 import { program } from "@caporal/core";
 
 program
-  .argument("<local_path>", "Local path of the static website build")
+  .argument("<local_path>", "Local path of the static website build", {
+    validator: program.STRING,
+  })
   .argument("<s3_bucket_id>", "ID of the target S3 bucket")
   .option(
     "--dry",
     "Do not actually upload files, simply show what would change"
   )
-  .action(({ logger, args, options }) => {
+  .action(async ({ logger, args, options }) => {
     logger.info(`args: ${JSON.stringify(args)}`);
     logger.info(`options: ${JSON.stringify(options)}`);
+    const files = await listFilesRecursively(args.localPath as string, logger);
+    logger.info(`files: ${JSON.stringify(files)}`);
   });
 
 program.run();
