909e008be398550b2e667e2c5a250c03bd1e4d5c
Philippe Muller <philippe.muller@gmail.com>
2022-03-15 09:55:41 +0100

    Add basic CLI args and options

--- packages/s3-static-site-uploader/src/index.ts
+++ packages/s3-static-site-uploader/src/index.ts
@@ -1,8 +1,16 @@
 /* eslint-disable import/no-unused-modules */
 import { program } from "@caporal/core";
 
-program.action(({ logger }) => {
-  logger.info("Hello, world!");
-});
+program
+  .argument("<local_path>", "Local path of the static website build")
+  .argument("<s3_bucket_id>", "ID of the target S3 bucket")
+  .option(
+    "--dry",
+    "Do not actually upload files, simply show what would change"
+  )
+  .action(({ logger, args, options }) => {
+    logger.info(`args: ${JSON.stringify(args)}`);
+    logger.info(`options: ${JSON.stringify(options)}`);
+  });
 
 program.run();
