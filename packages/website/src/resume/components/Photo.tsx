70d887af74ca3c07921cad613ca60f80cf01eb95
Philippe Muller <philippe.muller@gmail.com>
2022-10-15 11:38:29 +0200

    resume: Show Avatar instead of Photo

--- packages/website/src/resume/components/Photo.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-import PhotoJpg from "./Photo.jpg";
-import { PhotoContainer } from "./PhotoContainer";
-
-export const Photo: React.FC = () => (
-  <PhotoContainer src={PhotoJpg} alt="Philippe Muller" />
-);
