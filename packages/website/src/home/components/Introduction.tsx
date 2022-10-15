6778746754c7badeac50b25743eff50cbc758dc8
Philippe Muller <philippe.muller@gmail.com>
2022-10-15 11:42:08 +0200

    Simplify introduction

--- packages/website/src/home/components/Introduction.tsx
+++ packages/website/src/home/components/Introduction.tsx
@@ -10,12 +10,6 @@ export const Introduction: React.FC = () => (
     </p>
     <p>
       I love <Link href="/resume/">building software systems at scale</Link>.
-      Nowadays I write serverless apps using TypeScript with React on the
-      frontend and Pulumi to deploy the AWS backend.
-    </p>
-    <p>
-      Internet people taught me a lot and I am always happy to help. Feel free
-      to contact me if you'd like to discuss your projects.
     </p>
   </IntroductionContainer>
 );
